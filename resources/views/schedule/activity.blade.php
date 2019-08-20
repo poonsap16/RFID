7a05f59ce5e17f14176c465d67a626d4a1cb2eba
poonsap16 <kengp16@hotmail.com>
2019-08-20 21:00:38 +0700

    Create Project

---
+++ resources/views/schedule/activity.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layout')
+
+@section('title','RFID - Schedule Page')
+
+@section('content')
+<hr>
+<form>
+	<div class="form-group">
+	    <h4>
+	    	รายการตารางกิจกรรม
+	    </h4>
+	  
+	</div>
+	  <button type="submit" class="btn btn-primary">Submit</button>	
+
+</form>
+
+@endsection
