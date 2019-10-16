d3ac3d66982302fc40e6bf8283685a6cc42e5f0e
nongnapat2017 <nongnapat@gmail.com>
2019-10-16 12:53:44 +0700

    start event click

--- resources/views/calendars/calendar.blade.php
+++ resources/views/calendars/calendar.blade.php
@@ -2,14 +2,6 @@
 <link href='/css/fullcalendar4.3.1/daygrid/main.css' rel='stylesheet' />
 <link href='/css/fullcalendar4.3.1/timegrid/main.css' rel='stylesheet' />
 <style>
-
-  /* body {
-    margin: 40px 10px;
-    padding: 0;
-    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    font-size: 14px;
-  } */
-
   #calendar {
     margin: 40px 10px;
     padding: 0;
@@ -18,7 +10,6 @@
     /* max-width: 900px; */
     margin: 0 auto;
   }
-
 </style>
 
 <div id='calendar'></div>
@@ -29,7 +20,6 @@
 <script src='/css/fullcalendar4.3.1/timegrid/main.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.js'></script>
 <script>
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
@@ -44,18 +34,6 @@
       navLinks: true, // can click day/week names to navigate views
       selectable: true,
       selectMirror: true,
-      select: function(arg) {
-        var title = prompt('Event Title:');
-        if (title) {
-          calendar.addEvent({
-            title: title,
-            start: arg.start,
-            end: arg.end,
-            allDay: arg.allDay
-          })
-        }
-        calendar.unselect()
-      },
       editable: true,
       eventLimit: true, // allow "more" link when too many events
       events: function(info, successCallback, failureCallback) {
@@ -75,6 +53,9 @@
                   successCallback(row)
               };
           })
+      },
+      eventClick: function(calEvent, jsEvent, view) {
+        alert("event id => " + calEvent.event.id)
       }
     });
     calendar.render();
